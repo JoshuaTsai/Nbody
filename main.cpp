5a151cfa3d20073f25a9e64e53cf6c92d8c44155
Arpit Patel <arpit@Arpits-MacBook-Pro.local>
2018-04-22 14:55:24 -0400

    first

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+vel_y#include <stdio.h>
 #include <stdlib.h>
 
 #include <string.h>
@@ -11,17 +11,35 @@
 #include "properties.h"
 
 #define epsilon 0.000000000000000222
-#define DATATYPE float
-#define G 9.18
-/*
-vec3 *global_arr_MassSpeed; 
 
-//note--> vec3 is X,Y and mass not X, Y and Z
+#define g_constant (6.673e-11)
 
-void Initialize_Masses_and_Speeds(int light, int med, int heavy, vec3& global_arr_MassSpeed);
-*/
+double * pos_x;
+double * pos_y;
+double * vel_x;
+double * vel_y;
+double * mass;
+
+
+double ForceCalc(int x1, int y1, int x2, int y2, int m1, int m2, int xory){
+	double total_force = 0;
+	int dx = x1-x2;
+	int dy = y1-y2;
+	int dist = sqrt(dx*dx+dy*dy);
+	int 3dist = dist*dist*dist;
+
+	/* COMPUTE FORCE */
+	if(xory == 0){
+		total_force = -g*w1*w1/3dist*dx;
+	} else if (xory == 1){
+		total_force = -g*w1*w1/3dist*dy;
+	}
+	return total_force;
+}
+}
+
 int main(int argc, char* argv[]){
-	
+
 	if( argc != 10){
 		printf("Usage: %s numParticlesLight numParticleMedium numParticleHeavy numSteps subSteps timeSubStep imageWidth imageHeight imageFilenamePrex\n", argv[0]);
 	}
@@ -34,24 +52,83 @@ int main(int argc, char* argv[]){
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
 	//variables
-	int numParticlesLight = argv[1];
-	int numParticleMedium = argv[2];
-	int numParticleHeavy = argv[3];
-	int numSteps = argv[4];
-	int subSteps = argv[5];
-	double timeSubStep = argv[6];
-	int width = argv[7], height = argv[8];
+	int numParticlesLight = stoi(argv[1]);
+	int numParticleMedium = stoi(argv[2]);
+	int numParticleHeavy = stoi(argv[3]);
+	int width = stoi(argv[7]);
+	int height = stoi(argv[8]);
 
+	int totalParticles = lightParticles + mediumParticles + heavyParticles;
+
+	int numSteps = 0;
+	int subSteps = 0;
+	double timeSubStep;
+	int particles_torecv, particle_perproc = numParticlesTotal/p, particle_left = numParticlesTotal%p;
 	unsigned char* image;
-
-
-	const int total_particles = numParticlesLight + numParticleMedium + numParticleHeavy;
-	vec3 local_pos = new vec3[total_particles/p]; // assuming p evenly divides n.. 
-	vec3 temp_pos = new vec3[total_particles/p];
 
 	//root node stuff goes here
 	if(my_rank == 0){
-		//Initialize_Masses_and_Speeds(numParticlesLight, numParticleMedium, numParticleHeavy, global_arr_MassSpeed);
+
+		pos_x = (double *) malloc(sizeof(double) * numParticlesTotal);
+		pos_y = (double *) malloc(sizeof(double) * numParticlesTotal);
+		vel_x = (double *) malloc(sizeof(double) * numParticlesTotal);
+		vel_y = (double *) malloc(sizeof(double) * numParticlesTotal);
+		mass = (double *) malloc(sizeof(double) * numParticlesTotal);
+
+		for(i = 0; i < numParticlesTotal; i++){
+			if(i < numParticlesLight){
+				mass[i] = 1;
+				pos_x[i] = drand48()*width;
+				pos_y[i] = drand48()*height;
+				vel_x[i] = drand48();
+				vel_y[i] = drand48();
+				numParticlesTotal--;
+			} else if(i >= numParticlesLight && i < (numParticlesLight+numParticlesMedium)){
+				mass[i] = 2;
+				pos_x[i] = drand48()*width;
+				pos_y[i] = drand48()*height;
+				vel_x[i] = drand48();
+				vel_y[i] = drand48();
+				numParticlesTotal--;
+			} else{
+				mass[i] = 3;
+				pos_x[i] = drand48()*width;
+				pos_y[i] = drand48()*height;
+				vel_x[i] = drand48();
+				vel_y[i] = drand48();
+				numParticlesTotal--;
+			}
+		}
+
+
+		for (int outp = 0; outp < p; outp++){
+
+		//particles for each processor
+			if (outp < particle_left) {
+				particles_torecv = particle_perproc + 1;
+			}
+			else {
+				particles_torecv = particle_perproc;
+			}
+
+			local_mass = (int *) malloc(sizeof(int) * particles_torecv);
+			loc_arr_src_x = (int *) malloc(sizeof(int) * particles_torecv);
+			loc_arr_dst_x = (double *) malloc(sizeof(double) * particles_torecv);
+			loc_arr_src_y = (int *) malloc(sizeof(int) * particles_torecv);
+			loc_arr_dst_y = (double *) malloc(sizeof(double) * particles_torecv);
+			loc_arr_pointer = (int *) malloc(sizeof(int) * particles_torecv);
+
+			temp_mass = (int *) malloc(sizeof(int) * particles_torecv);
+			temp_arr_src_x = (int *) malloc(sizeof(int) * particles_torecv);
+			temp_arr_dst_x = (double *) malloc(sizeof(double) * particles_torecv);
+			temp_arr_src_y = (int *) malloc(sizeof(int) * particles_torecv);
+			temp_arr_dst_y = (double *) malloc(sizeof(double) * particles_torecv);
+			temp_arr_pointer = (int *) malloc(sizeof(int) * particles_torecv);
+
+
+			compute_pos_x = (int *) malloc(sizeof(int) * particles_torecv);
+			compute_pos_y = (int *) malloc(sizeof(int) * particles_torecv);
+
 
 
 		//almost done, just save the image
@@ -59,41 +136,11 @@ int main(int argc, char* argv[]){
 	}
 	//all other nodes do this
 	else{
-		source = (my_rank+1)%p;
-		dest = (my_rank-1+p)%p;
+
 	}
 
 	free(image);
 
 	MPI_Finalize();
 	return 0;
-}
-/*
-void Initialize_Masses_and_Speeds(int light, int med, int heavy, vec3& global_arr_MassSpeed)
-{
-	global_arr_MassSpeed = new vec3[light+med+heavy];
-
-	int iter = 0;
-	for(int i = 0; i < light; i++){
-		arr[iter++].x = velocityLightMin+drand48()*MAX_MIN_DIFF;
-		arr[iter++].y = velocityLightMin+drand48()*MAX_MIN_DIFF;
-		arr[iter++].z = massLightMin+drand48()*MAX_MIN_DIFF; //mass
-	}
-	for(int j = 0; j < med; j++){
-		arr[iter++].x = velocityMediumMin+drand48()*MAX_MIN_DIFF;
-		arr[iter++].y = velocityMediumMin+drand48()*MAX_MIN_DIFF;
-		arr[iter++].z = massMediumMin+drand48()*MAX_MIN_DIFF; //mass
-	}
-	for(int k = 0; k < heavy; k++){
-		arr[iter++].x = velocityHeavyMin+drand48()*MAX_MIN_DIFF;
-		arr[iter++].y = velocityHeavyMin+drand48()*MAX_MIN_DIFF;
-		arr[iter++].z = massHeavyMin+drand48()*MAX_MIN_DIFF; //mass
-	}
-}*/
-
-double Calculate_Force(DATATYPE x1_pos, DATATYPE x2_pos, DATATYPE y1_pos, DATATYPE y2_pos,\
-DATATYPE x1_vel, DATATYPE x2_vel, DATATYPE y1DATATYPE m1, DATATYPE m2, ){
-
-
-
 }
